7da0d06e877242845298f057433b1e0ee2878d46
Xenon_xod <dipdeb55@gmail.com>
2022-05-23 03:41:43 +0600

    delete api added

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ async function run() {
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query)
+            res.send(result)
         })
 
     }
